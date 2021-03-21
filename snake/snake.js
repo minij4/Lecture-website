a244766eeed92be8fe2aada98d0e1cbbddffc86c
Emilija <emilijaskirmantaite@gmail.com>
2021-03-21 18:41:20 +0200

    Adding dynamit, scissors, food and some other things

--- snake/snake.js
+++ snake/snake.js
@@ -1,25 +1,24 @@
+//kintamieji
 const N = 15;
 var X = 5;//gyvatės ilgis
-var red = 0;
-var RED = [];
-
+var snk = 0;
+var SNK = [];
+//maistas
 var food = 0;
 var foodId;
-
 //kliutims
 var OBS = [];
 var obs = 0;
 var putRock = 0;
-
 //taskam
-
 var score = 0;
-
 //zirkles
-
 var sciId;
 var sci = 0;
-
+// dinamitas
+var dynId;
+var dyn = 0;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function Table(){
     for(let i = 0; i < N; i++){
         let row1 = document.createElement('tr');
@@ -31,185 +30,180 @@ function Table(){
         }
     }
 }
-function generateTable(){
-    let table = document.createElement('table');
-    table.id = "fftable";//lentelės id
-    document.getElementById("ffTable").appendChild(table);
-    
-    Table();
-}
 function endGame(event){
     document.body.innerHTML = '<p id="gameOver">GAME OVER</p>';
     document.body.innerHTML += '<p id="result">Your score: ' + score + '</p>';
     document.body.innerHTML += '<center><a id="restartButton" href="snake.html">RESTART</a></center>';
 }
-//kartais atsiranda ant akmens
 function makeFood(){
-    if(food == 0){
-        foodId = 'col' + Math.floor(Math.random()*11) + Math.floor(Math.random()*11);
-        for(let i = 0; i < RED.length; i++){
-            while(RED[i] == foodId || OBS[i] == foodId){
-                foodId = 'col' + Math.floor(Math.random()*11) + Math.floor(Math.random()*11);
-            }
+    if(food == 0 && dyn == 0 && sci ==0){
+        foodId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
+        while(SNK.indexOf(foodId) !== -1 || OBS.indexOf(foodId) !== -1){
+            foodId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
         }
+        var rand = Math.floor(Math.random()*4)
 
-        document.getElementById(foodId).style.backgroundImage = 'url("./image/icecream1.png")';
+        if(rand == 0){
+            document.getElementById(foodId).style.backgroundImage = 'url("./image/icecream.png")';
+        }else if(rand == 1){
+            document.getElementById(foodId).style.backgroundImage = 'url("./image/berries.png")';
+        }else if(rand == 2){
+            document.getElementById(foodId).style.backgroundImage = 'url("./image/hamburger.png")';
+        }else{
+            document.getElementById(foodId).style.backgroundImage = 'url("./image/frog.png")';
+        }
         food = 1;
     }
 }
 function scissors(){
-    if(score % 20 == 0 && score > 0 && sci == 0){
-        sciId = 'col' + Math.floor(Math.random()*11) + Math.floor(Math.random()*11);
-
-        for(let i = 0; i < RED.length; i++){
-            while(RED[i] == sciId || OBS[i] == sciId){
-                sciId = 'col' + Math.floor(Math.random()*11) + Math.floor(Math.random()*11);
-            }
+    if(score % 50 == 0){
+        sciId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
+        while(SNK.indexOf(sciId) !== -1 || OBS.indexOf(sciId) !== -1){
+            sciId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
         }
         document.getElementById(sciId).style.backgroundImage = 'url("./image/scissors.png")';
-        sci=1;
-
+        sci = 1;
     }
 }
-function obst(){
+function obstacles(){
     if(score % 10 == 0){
-        var obstId = 'col' + Math.floor(Math.random()*11) + Math.floor(Math.random()*11);
-
-        for(let i = 0; i < RED.length; i++){
-            while(RED[i] == obstId || OBS[i] == obstId){
-                obstId = 'col' + Math.floor(Math.random()*11) + Math.floor(Math.random()*11);
-            }
+        var obstId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
+        while(SNK.indexOf(obstId) !== -1 || OBS.indexOf(obstId) !== -1){
+            obstId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
         }
-        
         document.getElementById(obstId).style.backgroundImage = 'url("./image/rock.png")';
         obs++;
         OBS.push(obstId);   
     }
 }
+function dynamit(){
+    if(score % 50 == 0){
+        dynId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
+        while(SNK.indexOf(dynId) !== -1 || OBS.indexOf(dynId) !== -1){
+            dynId = 'col' + Math.floor(Math.random()*(N+1)) + Math.floor(Math.random()*(N+1));
+        }
+        document.getElementById(dynId).style.backgroundImage = 'url("./image/dynamit.png")';
+        dyn = 1;
+    }
+}
 // uždeda paveiksliukus
-function head(){
+function snakeBody(){
+    for(let i = 0; i < SNK.length; i++){
+        if(i == SNK.length-1){
+            document.getElementById(SNK[i]).style.backgroundImage = 'url("./image/head3.png")';//galva
 
-    for(let i = 0; i < RED.length; i++){
-        if(i == RED.length-1){
-            document.getElementById(RED[i]).style.backgroundImage = 'url("./image/head3.png")';
         }else{
-            document.getElementById(RED[i]).style.backgroundImage = 'url("./image/tail3.png")';
+            document.getElementById(SNK[i]).style.backgroundImage = 'url("./image/tail3.png")';//uodega
         }
     }
 }
-function clearHead(){
-    document.getElementById(RED[RED.length - 1]).style.backgroundImage = 'url("")';
-}
-function turnRed(event){
+function Snake(event){
     //atsitrenkus į uodegą
-    for(let i = 0; i < RED.length; i++){
-        if(event.target.id == RED[i] && event.target.style.backgroundImage == 'url("./image/tail3.png")'){
+    if(event.target.id == SNK[SNK.length-2] && event.target.style.backgroundImage == 'url("./image/tail3.png")'){
+        return;
+    }
+    for(let i = 0; i < SNK.length; i++){
+        if(event.target.id == SNK[i] && event.target.style.backgroundImage == 'url("./image/tail3.png")'){
             endGame();
-            return;
         }
     }
-
-   
-    if(red > 0){
-        clearHead();
-    }
-
     //saugo gyvatės id
-    RED.push(event.target.id);
-    red++;
-    if(red > X){
-        document.getElementById(RED[0]).style.backgroundImage = 'url("")';
-        RED.shift();
-        red = X;
+    SNK.push(event.target.id);
+    snk++;
+    if(snk > X){
+        document.getElementById(SNK[0]).style.backgroundImage = 'url("")';
+        SNK.shift();
+        snk = X;
     }
-    
-    //maistas
-
     makeFood();
-
-
-    for(let i = 0; i < RED.length; i++){
-        if(RED[i] == foodId){
+    for(let i = 0; i < SNK.length; i++){
+        if(SNK[i] == foodId){
             score = score + 5;
-            
-            obst();
-            
+            //
+            obstacles();
+
+            var randNr = Math.floor(Math.random()*10);
+            if(randNr % 2 == 0){
+                scissors();
+            }else{
+                dynamit();
+            }//
             document.getElementById("score").innerHTML = "score: " + score;
             X++;
             food = 0;
-            
+
             document.getElementById(foodId).style.backgroundImage = 'url("")';
+            foodId = 0;
+        }   
+    }
+    snakeBody(); 
+    //kliūtys
+    for(let i = 0; i < OBS.length; i++){
+        if(SNK.indexOf(OBS[i]) !== -1){
+            endGame();
         }
     }
-    head(); 
-
-    
-    for(let i = 0; i < RED.length; i++){
-        for(let j = 0; j < OBS.length; j++){
-            if(RED[i] == OBS[j]){
-                endGame();
-            }
-        }
-    }
-    
-    scissors();
-    ////neveikia
-    
-
+    //zirkles
     if(event.target.id == sciId && sci == 1){
         for(let i = 0; i < 5; i++){
-            document.getElementById(RED[i]).style.backgroundImage = 'url("")';
-
-            RED.pop();
+            document.getElementById(SNK[0]).style.backgroundImage = 'url("")';
+            SNK.shift();
         }
         score = score + 5;
         document.getElementById("score").innerHTML = "score: " + score;
 
         X = X - 5;
-        red = X;
-        sci = 0;
+        snk = X;
         sciId = 0;
+        sci = 0;
+    }
+    //dynamits
+    if(event.target.id == dynId && dyn == 1){
+
+        for(let i = 0; i < Math.floor(OBS.length * 80 / 100); i++){
+            document.getElementById(OBS[0]).style.backgroundImage = "";
+            OBS.shift();
+        }
+        score = score + 5;
+        document.getElementById("score").innerHTML = "score: " + score;
+
+        dynId = 0;
+        dyn = 0;
     }
 }
 function steps(){
     //išėjimas iš ribos
     document.getElementById('fftable').addEventListener('mouseleave', endGame);
-
-
     //fiksuoja pelės vedžiojimą
     for(let i = 0; i < N; i++){
         for(let j = 0; j < N; j++){
-            document.getElementById('col'+i+j).addEventListener('mouseover', turnRed);
+            document.getElementById('col'+i+j).addEventListener('mouseover', Snake);
         }
     }
 }
 //fiksuoja pirmą paspaudimą
 function startPoint(event){
     document.getElementById("snake").style.backgroundImage = 'url("")';
-    turnRed(event);//////////////////////////////////////////////////////////id
+    Snake(event);//////////////////////////////////////////////////////////id
     for(let i = 0; i < N; i++){
         for(let j = 0; j < N; j++){
             document.getElementById('col'+i+j).removeEventListener('click', startPoint);
         }
     }
-
     steps();
 }
 //uždeda visom ląstelėm galimybę būti pirmoms
-function setEvent(){
+function setStart(){
     for(let i = 0; i < N; i++){
         for(let j = 0; j < N; j++){
             document.getElementById('col'+i+j).addEventListener('click', startPoint);
         }
     }
 }
-//pagrindinė funkcija
 function Game(){
-    generateTable();
-    setEvent();
+    Table();
+    setStart();
 }
 
 Game();
-
-
 
